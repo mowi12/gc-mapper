5856508fa2825c94b5758589005afb4bac8261ae
Moritz Wieland <moritz.wieland@googlemail.com>
2024-12-07 15:40:04 +0100

    feat: Support 3 variables

--- script.js
+++ script.js
@@ -19,13 +19,12 @@ const coordInput = document.getElementById("coordInput");
 function show() {
   clearResults();
 
-  // input does not contain any variables
-  if (!coordInput.value.includes("x")) {
+  const varsp = /[a-z]/g;
+  const count = (coordInput.value.match(varsp) || []).length;
+
+  if (count === 0) {
     convertCoordinatesNoVars();
-    return;
   } else {
-    // input contains variables
-    const count = (coordInput.value.match(/x/g) || []).length;
     convertCoordinatesWithVars(count);
   }
 }
@@ -33,7 +32,7 @@ function show() {
 function showPoint(lat, lng) {
   const pt = L.marker([lat, lng])
     .addTo(map)
-    .bindPopup(`${convertToDegrees(lat)} ${convertToDegrees(lng)}`);
+    .bindPopup(`${convertToDegrees(lat)}' ${convertToDegrees(lng)}'`);
 
   map.setView([lat, lng], zoom);
 }
@@ -57,7 +56,7 @@ function showLine(lat1, lng1, lat2, lng2) {
   map.setView(center, zoom);
 }
 
-function showRect(lat1, lng1, lat2, lng2) {
+function showRect(lat1, lng1, lat2, lng2, text) {
   const rect = L.rectangle(
     [
       [lat1, lng1],
@@ -67,7 +66,9 @@ function showRect(lat1, lng1, lat2, lng2) {
       color: "red",
       weight: 2,
     }
-  ).addTo(map);
+  )
+    .addTo(map)
+    .bindPopup(text);
 
   const center = [
     parseFloat(lat1) + (parseFloat(lat2) - parseFloat(lat1)) / 2,
@@ -114,26 +115,27 @@ function convertToDegrees(coordsInDecimal) {
 }
 
 function convertCoordinatesWithVars(count) {
-  const coordString = coordInput.value;
+  let coordString = coordInput.value;
   const regexp =
-    /((N|S)?)(\s?)(?<lat1>[0-9x]{1,3})(°?)(\s?)(?<lat2>[0-9x]{1,2}.[0-9x]{3})('?)(\s?)((E|W)?)(\s?)(?<lng1>[0-9x]{1,3})(°?)(\s?)(?<lng2>[0-9x]{1,2}.[0-9x]{3})('?)/g;
-  const matches = coordString.matchAll(regexp);
+    /((N|S)?)(\s?)(?<lat1>[0-9a-z]{1,3})(°?)(\s?)(?<lat2>[0-9a-z]{1,2}.[0-9a-z]{3})('?)(\s?)((E|W)?)(\s?)(?<lng1>[0-9a-z]{1,3})(°?)(\s?)(?<lng2>[0-9a-z]{1,2}.[0-9a-z]{3})('?)/g;
   if (count === 1) {
+    const variable = coordString.match(/[a-z]/g)[0];
+    const matches = Array.from(coordString.matchAll(regexp));
     for (const match of matches) {
       const latDeg1 = (match.groups.lat1 + " " + match.groups.lat2).replace(
-        "x",
+        variable,
         "0"
       );
       const lngDeg1 = (match.groups.lng1 + " " + match.groups.lng2).replace(
-        "x",
+        variable,
         "0"
       );
       const latDeg2 = (match.groups.lat1 + " " + match.groups.lat2).replace(
-        "x",
+        variable,
         "9"
       );
       const lngDeg2 = (match.groups.lng1 + " " + match.groups.lng2).replace(
-        "x",
+        variable,
         "9"
       );
       const latDec1 = convertToDecimal(latDeg1);
@@ -145,21 +147,23 @@ function convertCoordinatesWithVars(count) {
       showLine(latDec1, lngDec1, latDec2, lngDec2);
     }
   } else if (count === 2) {
+    const variables = coordString.match(/[a-z]/g);
+    const matches = Array.from(coordString.matchAll(regexp));
     for (const match of matches) {
       const latDeg1 = (match.groups.lat1 + " " + match.groups.lat2).replace(
-        "x",
+        variables[0],
         "0"
       );
       const lngDeg1 = (match.groups.lng1 + " " + match.groups.lng2).replace(
-        "x",
+        variables[1],
         "0"
       );
       const latDeg2 = (match.groups.lat1 + " " + match.groups.lat2).replace(
-        "x",
+        variables[0],
         "9"
       );
       const lngDeg2 = (match.groups.lng1 + " " + match.groups.lng2).replace(
-        "x",
+        variables[1],
         "9"
       );
       const latDec1 = convertToDecimal(latDeg1);
@@ -171,6 +175,54 @@ function convertCoordinatesWithVars(count) {
       showPoint(latDec1, lngDec2);
       showPoint(latDec2, lngDec1);
       showRect(latDec1, lngDec1, latDec2, lngDec2);
+    }
+  } else if (count === 3) {
+    const variables = coordString.match(/[a-z]/g);
+    for (let i = 0; i < count - 2; i++) {
+      for (let value = 0; value < 10; value++) {
+        let coordStringCopy = coordString.replace(variables[i], value);
+        const matches = Array.from(coordStringCopy.matchAll(regexp));
+        for (const match of matches) {
+          const latDeg1 = (match.groups.lat1 + " " + match.groups.lat2)
+            .replace(variables[count - 1], "0")
+            .replace(variables[count - 2], "0");
+          const lngDeg1 = (match.groups.lng1 + " " + match.groups.lng2)
+            .replace(variables[count - 1], "0")
+            .replace(variables[count - 2], "0");
+          const latDeg2 = (match.groups.lat1 + " " + match.groups.lat2)
+            .replace(variables[count - 1], "9")
+            .replace(variables[count - 2], "9");
+          const lngDeg2 = (match.groups.lng1 + " " + match.groups.lng2)
+            .replace(variables[count - 1], "9")
+            .replace(variables[count - 2], "9");
+          const latDec1 = convertToDecimal(latDeg1);
+          const lngDec1 = convertToDecimal(lngDeg1);
+          const latDec2 = convertToDecimal(latDeg2);
+          const lngDec2 = convertToDecimal(lngDeg2);
+          showPoint(latDec1, lngDec1);
+          showPoint(latDec2, lngDec2);
+          showPoint(latDec1, lngDec2);
+          showPoint(latDec2, lngDec1);
+          showRect(
+            latDec1,
+            lngDec1,
+            latDec2,
+            lngDec2,
+            `${variables[i]} = ${value}, ${variables[count - 2]} and ${
+              variables[count - 1]
+            } from 0 to 9`
+          );
+          // console.log("variable: " + variables[i] + " has value: " + value);
+          // console.log("Replacing: " + variables[count - 1] + " with 0 and 9");
+          // console.log("Replacing: " + variables[count - 2] + " with 0 and 9");
+          // console.log("----");
+          // console.log("latDeg1: " + latDeg1 + " lngDeg1: " + lngDeg1);
+          // console.log("latDeg2: " + latDeg2 + " lngDeg2: " + lngDeg2);
+          // console.log("latDec1: " + latDec1 + " lngDec1: " + lngDec1);
+          // console.log("latDec2: " + latDec2 + " lngDec2: " + lngDec2);
+          // console.log("\n");
+        }
+      }
     }
   }
 }
