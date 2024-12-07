4b828147577e4ba12de4d23a8f471ffbe05895d7
Moritz Wieland <moritz.wieland@googlemail.com>
2024-12-07 11:59:36 +0100

    feat: Implement functionality for 0, 1 or 2 variables

--- script.js
+++ script.js
@@ -14,17 +14,20 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 /* Input */
 const coordInput = document.getElementById("coordInput");
-let latDec,
-  lngDec,
-  latDeg,
-  lngDeg = 0;
 /* ----- */
 
 function show() {
-  convertCoordinates();
   clearResults();
 
-  showPoint(latDec, lngDec);
+  // input does not contain any variables
+  if (!coordInput.value.includes("x")) {
+    convertCoordinatesNoVars();
+    return;
+  } else {
+    // input contains variables
+    const count = (coordInput.value.match(/x/g) || []).length;
+    convertCoordinatesWithVars(count);
+  }
 }
 
 function showPoint(lat, lng) {
@@ -43,7 +46,7 @@ function showLine(lat1, lng1, lat2, lng2) {
     ],
     {
       color: "green",
-      weight: 5,
+      weight: 2,
     }
   ).addTo(map);
 
@@ -62,7 +65,7 @@ function showRect(lat1, lng1, lat2, lng2) {
     ],
     {
       color: "red",
-      weight: 5,
+      weight: 2,
     }
   ).addTo(map);
 
@@ -81,20 +84,17 @@ function clearResults() {
   });
 }
 
-function convertCoordinates() {
+function convertCoordinatesNoVars() {
   const coordString = coordInput.value;
   const regexp =
-    /((N|S)?)(\s?)(?<lat1>[0-9x]{1,3})(°?)(\s?)(?<lat2>[0-9x]{1,2}.[0-9x]{3})('?)(\s?)((E|W)?)(\s?)(?<lng1>[0-9x]{1,3})(°?)(\s?)(?<lng2>[0-9x]{1,2}.[0-9x]{3})('?)/g;
+    /((N|S)?)(\s?)(?<lat1>[0-9]{1,3})(°?)(\s?)(?<lat2>[0-9]{1,2}.[0-9]{3})('?)(\s?)((E|W)?)(\s?)(?<lng1>[0-9]{1,3})(°?)(\s?)(?<lng2>[0-9]{1,2}.[0-9]{3})('?)/g;
   const matches = coordString.matchAll(regexp);
   for (const match of matches) {
-    latDeg = match.groups.lat1 + " " + match.groups.lat2;
-    lngDeg = match.groups.lng1 + " " + match.groups.lng2;
-    latDec = convertToDecimal(latDeg);
-    lngDec = convertToDecimal(lngDeg);
-    console.log("latDeg: " + latDeg);
-    console.log("lngDeg: " + lngDeg);
-    console.log("latDec: " + latDec);
-    console.log("lngDec: " + lngDec);
+    const latDeg = match.groups.lat1 + " " + match.groups.lat2;
+    const lngDeg = match.groups.lng1 + " " + match.groups.lng2;
+    const latDec = convertToDecimal(latDeg);
+    const lngDec = convertToDecimal(lngDeg);
+    showPoint(latDec, lngDec);
   }
 }
 
@@ -107,4 +107,66 @@ function convertToDegrees(coordsInDecimal) {
   const degrees = Math.floor(coordsInDecimal);
   const minutes = (coordsInDecimal - degrees) * 60;
   return degrees + " " + minutes.toFixed(3);
+}
+
+function convertCoordinatesWithVars(count) {
+  const coordString = coordInput.value;
+  const regexp =
+    /((N|S)?)(\s?)(?<lat1>[0-9x]{1,3})(°?)(\s?)(?<lat2>[0-9x]{1,2}.[0-9x]{3})('?)(\s?)((E|W)?)(\s?)(?<lng1>[0-9x]{1,3})(°?)(\s?)(?<lng2>[0-9x]{1,2}.[0-9x]{3})('?)/g;
+  const matches = coordString.matchAll(regexp);
+  if (count === 1) {
+    for (const match of matches) {
+      const latDeg1 = (match.groups.lat1 + " " + match.groups.lat2).replace(
+        "x",
+        "0"
+      );
+      const lngDeg1 = (match.groups.lng1 + " " + match.groups.lng2).replace(
+        "x",
+        "0"
+      );
+      const latDeg2 = (match.groups.lat1 + " " + match.groups.lat2).replace(
+        "x",
+        "9"
+      );
+      const lngDeg2 = (match.groups.lng1 + " " + match.groups.lng2).replace(
+        "x",
+        "9"
+      );
+      const latDec1 = convertToDecimal(latDeg1);
+      const lngDec1 = convertToDecimal(lngDeg1);
+      const latDec2 = convertToDecimal(latDeg2);
+      const lngDec2 = convertToDecimal(lngDeg2);
+      showPoint(latDec1, lngDec1);
+      showPoint(latDec2, lngDec2);
+      showLine(latDec1, lngDec1, latDec2, lngDec2);
+    }
+  } else if (count === 2) {
+    for (const match of matches) {
+      const latDeg1 = (match.groups.lat1 + " " + match.groups.lat2).replace(
+        "x",
+        "0"
+      );
+      const lngDeg1 = (match.groups.lng1 + " " + match.groups.lng2).replace(
+        "x",
+        "0"
+      );
+      const latDeg2 = (match.groups.lat1 + " " + match.groups.lat2).replace(
+        "x",
+        "9"
+      );
+      const lngDeg2 = (match.groups.lng1 + " " + match.groups.lng2).replace(
+        "x",
+        "9"
+      );
+      const latDec1 = convertToDecimal(latDeg1);
+      const lngDec1 = convertToDecimal(lngDeg1);
+      const latDec2 = convertToDecimal(latDeg2);
+      const lngDec2 = convertToDecimal(lngDeg2);
+      showPoint(latDec1, lngDec1);
+      showPoint(latDec2, lngDec2);
+      showPoint(latDec1, lngDec2);
+      showPoint(latDec2, lngDec1);
+      showRect(latDec1, lngDec1, latDec2, lngDec2);
+    }
+  }
 }
